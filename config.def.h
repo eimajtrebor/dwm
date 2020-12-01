142d8f778d9939920f48863f8f18541163c6050d
Jim McCormick <jim.mccormick@sbvonline.co.uk>
2020-12-01 18:48:32 +0000

    update config

--- config.def.h
+++ config.def.h
@@ -1,11 +1,15 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Roboto Mono-10" };
+static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const char *fonts[]          = {
+    "Roboto Mono-10",
+    "Symbols Nerd Font-10"
+};
 static const char dmenufont[]       = "Roboto Mono-10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
